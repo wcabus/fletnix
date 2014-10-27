1b8e6c4821e155a2d2b35a4f9832e6693eea6e94
Wesley Cabus <wesley@gotsharp.be>
2014-10-27 12:23:48 +0100

    Added ImageUri field

--- Fletnix/Fletnix.Domain/TvShow.cs
+++ Fletnix/Fletnix.Domain/TvShow.cs
@@ -12,6 +12,9 @@ namespace Fletnix.Domain
         [Required, StringLength(256)]
         public string Title { get; set; }
 
+        [StringLength(1024)]
+        public string ImageUri { get; set; }
+
         public virtual ICollection<Genre> Genres { get; set; }
         public virtual ICollection<ShowSeason> Seasons { get; set; }
     }
